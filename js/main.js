007d3d7fb608a214d6d3e8898707438c574d2b5a
Mathieu Mayer-Mazzoli <mathieu.mayermazzoli@gmail.com>
2018-03-31 10:46:04 +0900

    added in responsive breakpoints

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 
   // Reveal objects on scroll
-    window.sr = ScrollReveal();
+    window.sr = ScrollReveal({ mobile: false });
     sr.reveal('.work-list-item', { duration: 1000, easing: 'ease-out', scale: 1, distance: '150px', viewFactor: 0.5, });
 
 
   // Screen transitions
+  if ($(window).width() > 639) {
+    Barba.Pjax.start();
 
-  Barba.Pjax.start();
-
-  Barba.Prefetch.init();
+    Barba.Prefetch.init();
+  }
 
 });
