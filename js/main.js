11c77d218bedac8fddcd12777ff03c30ec70eca9
Mathieu Mayer-Mazzoli <mathieu.mayermazzoli@gmail.com>
2018-04-05 22:29:13 +0900

    set transition length in js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 
   // Reveal objects on scroll
-    window.sr = ScrollReveal({ mobile: false });
-    sr.reveal('.work-list-item', { duration: 500, easing: 'ease-out', scale: 0.9, distance: '100px', viewFactor: 0.5, });
+  window.sr = ScrollReveal({ mobile: false });
+  sr.reveal('.work-list-item', {
+    duration: 500,
+    easing: 'ease-out',
+    scale: 0.9,
+    distance: '100px',
+    viewFactor: 0.5,
+  });
 
 
   // Screen transitions
-    Barba.Pjax.start();
+  Barba.Pjax.start();
+
+  Barba.transitionLength = 500;
 
   if ($(window).width() > 639) {
     Barba.Prefetch.init();
